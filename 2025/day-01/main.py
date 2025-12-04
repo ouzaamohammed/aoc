bb1cbdd14562b98187b27290943873c926c4534b
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-12-04 17:32:19 +0100

    day 01 part two solution

--- 2025/day-01/main.py
+++ 2025/day-01/main.py
@@ -1,33 +1,33 @@
 def main():
     file_path = "input.txt"
-    start = 50
-    count = 0
+    pos = 50
+    zero_count = 0
+    zero_touched_count = 0
     try:
         with open(file_path, "r") as f:
             lines = f.readlines()
-            curr = start
             for line in lines:
                 if not line:
                     continue
-
                 dir = line[0]
                 dist = int(line[1:])
-
-                if dir == "L":
-                    curr = (curr - dist) % 100
-                    if curr == 0:
-                        count += 1
-                if dir == "R":
-                    curr = (curr + dist) % 100
-                    if curr == 0:
-                        count += 1
+                for _ in range(dist):
+                    if dir == "L":
+                        pos = (pos - 1 + 100) % 100
+                    else:
+                        pos = (pos + 1) % 100
+                    if pos == 0:
+                        zero_touched_count += 1
+                if pos == 0:
+                    zero_count += 1
 
     except FileNotFoundError:
         print(f"Error: the file '{file_path}' was not found.")
     except Exception as e:
         print(f"An error accured: {e}")
 
-    print(count)
+    print(f"Part one solution: {zero_count}")
+    print(f"Part two solution: {zero_touched_count}")
 
 
 main()
